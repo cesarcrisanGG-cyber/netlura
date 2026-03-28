7119115e9b92736aadb060966cc0408bf9944dc0
GGrare <cesarcrisan705@gmail.com>
2026-03-28 20:06:04 -0300

    ATT

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -3,19 +3,19 @@ export const categories = [
         title: "Épicos",
         items: [
             {
-                img: "https://images8.alphacoders.com/565/thumb-1920-565975.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                img: "../image/img-1.webp", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=m1lZWAwC5Bw"
             },
             {
-                img: "https://www.sociedadenerd.com.br/wp-content/uploads/2022/08/The-Walking-Dead-8-temporada.jpg", progress: 0,
+                img: "../image/img-2.webp", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=o-L1mMZ31hM"
             },
             {
-                img: "https://tse2.mm.bing.net/th/id/OIP.Yf5tETvduHnOorIpVRsoLAHaFl?rs=1&pid=ImgDetMain&o=7&rm=3", progress: 0,
+                img: "../image/img-3.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=_LrhrW5-Afs"
             },
             {
-                img: "https://www.clarin.com/img/2019/10/24/A9ZAwfJM_1256x620__1.jpg", progress: 0,
+                img: "../image/img-4.jpg", progress: 0,
                 youtube: "https://www.youtube.com/watch?v=LJGVllhIcWE"
             },
         ]
@@ -23,19 +23,19 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/11/06214526/The-walking-dead-2.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=cKl61k4OCSI" },
-            { img: "https://2.bp.blogspot.com/-MJd5Ef9D0fU/UgAVzz2sgEI/AAAAAAAAALo/gPw8OVYD2q4/s1600/the_walking_dead_tercera_temporada.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=B__2hnNQlEQ" },
-            { img: "https://es.web.img3.acsta.net/pictures/18/06/04/13/40/5717001.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=-ELrDHaZ0ZM" },
-            { img: "https://ovicio.com.br/wp-content/uploads/2021/10/20211021-ovicio-the-walking-dead-season-9.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=1wHdx_zIBNA" },
+            { img: "../image/img-5.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=cKl61k4OCSI" },
+            { img: "../image/img-8.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=B__2hnNQlEQ" },
+            { img: "../image/img-7.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=-ELrDHaZ0ZM" },
+            { img: "../image/img-09.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=1wHdx_zIBNA" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://i0.wp.com/www.moonmagazine.info/wp-content/uploads/2017/10/the_walking_dead_claves_portada_moonmagazine.jpg?fit=1920%2C1080&ssl=1", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://th.bing.com/th/id/R.daba5480df0e111163675319e258438d?rik=n1eDsXkQMRc9TA&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2f9%2f2%2fd%2f1342698-widescreen-twd-wallpaper-season-6-1920x1080-high-resolution.jpg&ehk=7E%2bvPGTqIWHqIGl32XENt2AGUaJSXZuKClSFc4e1Pdo%3d&risl=&pid=ImgRaw&r=0", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://areajugones.sport.es/wp-content/uploads/2020/10/the-walking-dead.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://s.aficionados.com.br/imagens/primeira-temporada-do-the-walking-dead_cke.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../image/img-10.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../image/img-11.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../image/img-12.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "../image/img-4.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
         ]
     }
 ];
